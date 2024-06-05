6e142ce157d1e2543f3caad4033ed273412823b5
Thanhducdepzai <136815568+Thanhducdepzai@users.noreply.github.com>
2024-06-05 09:58:52 +0700

    Add project files.

---
+++ DemoProject/Pages/Result.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoProject.Pages
+{
+    public class ResultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
